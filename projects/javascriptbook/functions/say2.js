2b896f178993efb8bccd4bc9281ac89eda9da834
Kevin Lin <kevinlin56781@gmail.com>
2024-12-04 08:10:11 +0000

    'adding some left over exercises for JS prep section'

---
+++ projects/javascriptbook/functions/say2.js
@@ -0,0 +1,19 @@
+/* 
+
+console.log('hello');
+console.log('hi');
+console.log('how do you do');
+console.log('Quite all right'); 
+
+code before functions
+*/
+
+function say(text) {
+  console.log('==> ' + text);
+}
+
+say('hello');
+say('hi');
+say('how do you do');
+say('Quite all right');
+
